3d525154e6dcfe4b236b4043353c8c8b71361a83
Ivan Simunovic <ivan.simunovic9898@gmail.com>
2021-04-02 16:56:38 +0200

    remove-main-view

--- CoronaVirusApp/Module/View/ViewController/CountrySelectionViewController.swift
+++ CoronaVirusApp/Module/View/ViewController/CountrySelectionViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class CountrySelectionViewController: UIViewController {
     //MARK: Properties
-    let countrySelectionView: CountrySelectionMainView = {
-        let view = CountrySelectionMainView()
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.isTranslucent = true
+        searchBar.barTintColor = UIColor.clear
+        searchBar.backgroundColor = UIColor.clear
+        searchBar.backgroundImage = UIImage()
+        searchBar.placeholder = "Search..."
+        return searchBar
+    }()
+    
+    let horizontalDividor: UIView = {
+        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
         return view
+    }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        return tableView
     }()
     
     //MARK: Init
@@ -42,6 +61,7 @@ private extension CountrySelectionViewController {
         setupAppearance()
         addViews()
         setupLayout()
+        configureTableView()
     }
     
     func setupAppearance() {
@@ -49,12 +69,50 @@ private extension CountrySelectionViewController {
     }
     
     func addViews() {
-        view.addSubview(countrySelectionView)
+        let views = [searchBar, horizontalDividor, tableView]
+        view.addSubviews(views)
     }
     
     func setupLayout() {
-        countrySelectionView.snp.makeConstraints { (make) in
-            make.edges.equalTo(view.safeAreaLayoutGuide)
+        searchBar.snp.makeConstraints { (make) in
+            make.top.leading.trailing.equalToSuperview().inset(UIEdgeInsets(top: 16, left: 32, bottom: 0, right: 32))
         }
+        
+        horizontalDividor.snp.makeConstraints { (make) in
+            make.top.equalTo(searchBar.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(32)
+            make.height.equalTo(1)
+        }
+        
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(horizontalDividor.snp.bottom).offset(16)
+            make.leading.bottom.trailing.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 32, bottom: 0, right: 32))
+        }
+    }
+}
+
+//MARK: - TableView Config
+extension CountrySelectionViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let name = "Croatia"
+        
+        let cell: CountrySelectionTableViewCell = tableView.dequeue(for: indexPath)
+        
+        cell.configure(with: name)
+        
+        return cell
+    }
+    
+    func configureTableView() {
+        setTableViewDelegates()
+        tableView.register(CountrySelectionTableViewCell.self, forCellReuseIdentifier: CountrySelectionTableViewCell.reuseIdentifier)
+    }
+    func setTableViewDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
